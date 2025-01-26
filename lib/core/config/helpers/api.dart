e313168d3997216275d75c6330eb3f8b6d609ec1
daniel-zuluaga <daniel.zuluaga007@gmail.com>
2025-01-25 21:50:11 -0500

    Enhance API helper methods with detailed documentation for better clarity and maintainability

--- lib/core/config/helpers/api.dart
+++ lib/core/config/helpers/api.dart
@@ -7,24 +7,26 @@ import 'package:http/http.dart' as http;
 class Api {
   static final Map<String, String> _headers = {};
 
+  /// Sets the default headers for HTTP requests.
   static Future<void> setHeaders() async {
     _headers[HttpHeaders.contentTypeHeader] = 'application/json';
   }
 
+  /// Clears all headers previously set.
   static void clearHeaders() {
     _headers.clear();
   }
 
-  // Parsea un json en formato string a un Map
-  // Returns. Map String, dynamic
+  /// Parses a JSON string into a Map.
+  /// Returns: Map<String, dynamic>
   static dynamic decode(String response) => json.decode(response);
 
-  // Encode un json en formato Map a un String
-  // Returns. Map String, dynamic
+  /// Encodes a Map into a JSON string.
+  /// Returns: String
   static String encode(Map<String, dynamic> response) => json.encode(response);
 
-  //Realiza peticiones tipo GET al backend al endpoint pasado por parámetro
-  //Returns Future con el resultado de la consulta
+  /// Performs a GET request to the backend using the specified endpoint.
+  /// Returns: Future with the result of the request.
   static Future<dynamic> get(String path) async {
     await setHeaders();
     debugPrint('-------------GETHEADER--------------');
@@ -36,8 +38,9 @@ class Api {
     );
     return response;
   }
-  //Realiza peticiones tipo POST al backend al endpoint pasado por parámetro
-  //Returns Future con el resultado de la consulta
+
+  /// Performs a POST request to the backend using the specified endpoint and data.
+  /// Returns: Future with the result of the request.
   static Future<dynamic> post(String path, Map<String, dynamic> data) async {
     await setHeaders();
     debugPrint('---------------------------');
@@ -57,11 +60,8 @@ class Api {
     }
   }
 
-  // static Future<dynamic> postFormData(String path,Map<String, dynamic> data)async {
-  //   await setHeaders();
-
-  // }
-
+  /// Performs a PUT request to the backend using the specified endpoint and data.
+  /// Returns: Future with the result of the request.
   static Future<dynamic> put(String path, Map<String, dynamic> data) async {
     await setHeaders();
     debugPrint('---------------------------');
